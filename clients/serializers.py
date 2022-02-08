3812a9d28748242949f3f2b20d344fe9bdddfe8a
Klement Omeri <klementomeri97@gmail.com>
2022-02-09 00:31:08 +0100

    chore: handle integrity error when creating client

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 
 from clients.models import Client
@@ -8,8 +9,16 @@ class ClientSerializer(serializers.Serializer):
     name = serializers.CharField()
 
     def create(self, validated_data):
-        return Client.objects.create(**validated_data)
+        try:
+            return Client.objects.create(**validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {'number': ['Client with this number already exists.']}
+            )
 
     def update(self, instance, validated_data):
         instance.number = validated_data.get('number', instance.number)
         instance.name = validated_data.get('name', instance.name)
+        instance.save()
+
+        return instance
